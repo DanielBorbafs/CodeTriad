5699bb6388973ef1ce2e6fac5fbb40a47c3c08c7
DanielBorbafs <dev.danielborba@gmail.com>
2024-07-31 15:36:05 -0300

    Fix: fix bug function selection.js

--- assets/js/selection.js
+++ assets/js/selection.js
@@ -1,37 +1,23 @@
-// função para selecionar e trocar a borda dos items dos serviços
-// Pegando os elementos:
 
-const imgSistemas = document.querySelector('.img-sistemas')
-const imgUi = document.querySelector('.img-ui')
-const imgTrafego = document.querySelector('.img-trafego')
-const imgEcommerce = document.querySelector('.img-ecommerce')
-
-imgSistemas.addEventListener('click', () => {
-    removeBorda();
-    imgSistemas.classList.add('border-color')
-})
-
-imgUi.addEventListener('click', () => {
-    removeBorda();
-    imgUi.classList.add('border-color')
-})
-
-imgTrafego.addEventListener('click', () => {
-    removeBorda();
-    imgTrafego.classList.add('border-color')
-})
-
-imgEcommerce.addEventListener('click', () => {
-    removeBorda();
-    imgEcommerce.classList.add('border-color')
-})
-
-
+const iconServices = document.querySelectorAll('.icon-service');
 
 function removeBorda() {
-    imgSistemas.classList.remove('border-color')
-    imgUi.classList.remove('border-color')
-    imgTrafego.classList.remove('border-color')
-    imgEcommerce.classList.remove('border-color')
+    iconServices.forEach(service => {
+        service.querySelector('img').classList.remove('border-color');
+    });
 }
-//border-color
+
+iconServices.forEach(service => {
+    service.addEventListener('click', (event) => {
+        removeBorda();
+        service.querySelector('img').classList.add('border-color');
+    });
+
+    service.querySelectorAll('*').forEach(child => {
+        child.addEventListener('click', (event) => {
+            removeBorda();
+            service.querySelector('img').classList.add('border-color');
+        
+        });
+    });
+});
